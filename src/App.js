08dbe327342d38e7e414d6e91128aa6a5a6b557e
Tan <vohavinhtan6@gmail.com>
2023-03-01 23:56:39 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { publicRoutes } from './routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/elegant-icons.css'
+import './assets/css/font-awesome.min.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          {
+            publicRoutes.map((router, index) => {
+              const Page = router.component;
+              return (
+                <Route key={index} path={router.path} element={<Page />} />
+              )
+            })
+          }
+        </Routes>
+      </Router>
     </div>
   );
 }
