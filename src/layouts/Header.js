814669978a4f2ea40d6c65fb593e6748fb3f936e
Tan <vohavinhtan6@gmail.com>
2023-03-02 11:59:27 +0700

    ss

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -42,42 +42,41 @@ function Header() {
                         <div className="col-xl-6 col-lg-7">
                             <nav className="header__menu">
                                 <ul>
-                                    <li className="active"><Link to="./index.html">Home</Link></li>
-                                    <li><Link to="/">Women’s</Link></li>
-                                    <li><Link to="/">Men’s</Link></li>
-                                    <li><Link to="./shop.html">Shop</Link></li>
-                                    <li><Link to="/">Pages</Link>
+                                    <li className=""><Link to="/">Trang chủ</Link></li>
+                                    <li><Link to="/shopsilvers">Trang sức bạc</Link>
                                         <ul className="dropdown">
-                                            <li><Link to="./product-details.html">Product Details</Link></li>
-                                            <li><Link to="./shop-cart.html">Shop Cart</Link></li>
-                                            <li><Link to="./checkout.html">Checkout</Link></li>
-                                            <li><Link to="./blog-details.html">Blog Details</Link></li>
+                                            <li><Link to="">Nhẫn Bạc</Link></li>
+                                            <li><Link to="">Dây Chuyền Bạc</Link></li>
+                                            <li><Link to="">Lắc Tay Bạc</Link></li>
+                                            <li><Link to="">Bông Tai Bạc</Link></li>
                                         </ul>
                                     </li>
-                                    <li><Link to="./blog.html">Blog</Link></li>
-                                    <li><Link to="./contact.html">Contact</Link></li>
+                                    <li><Link to="/">Trang sức vàng</Link></li>
+                                    <li><Link to="">Kẹp cài tóc</Link></li>
+                                    <li><Link to="/blog">Blog</Link></li>
+                                    <li><Link to="/contact">Liên hệ</Link></li>
                                 </ul>
                             </nav>
                         </div>
-                        {/* <div className="col-lg-3">
+                        <div className="col-lg-3">
                             <div className="header__right">
                                 <div className="header__right__auth">
-                                    <Link to="/">Login</Link>
-                                    <Link to="/">Register</Link>
+                                    <Link to="/">Đăng nhập</Link>
+                                    <Link to="/">Đăng ký</Link>
                                 </div>
                                 <ul className="header__right__widget">
                                     <li><span className="icon_search search-switch">
                                     </span></li>
                                     <li><Link to="/"><span className="icon_heart_alt">
                                     </span>
-                                        <div className="tip">2</div>
+                                        <div className="tip">0</div>
                                     </Link></li>
                                     <li><Link to="/"><span className="icon_bag_alt"></span>
-                                        <div className="tip">2</div>
+                                        <div className="tip">0</div>
                                     </Link></li>
                                 </ul>
                             </div>
-                        </div> */}
+                        </div>
                     </div>
                     <div className="canvas__open">
                         <i className="fa fa-bars"></i>
